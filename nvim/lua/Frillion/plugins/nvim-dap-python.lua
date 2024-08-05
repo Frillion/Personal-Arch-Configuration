e6085da7d719d84825dad9159e127a390ac4ea1f
Frillion <fridriksoebech@gmail.com>
2024-08-05 21:26:59 +0000

    fixing submodule for main

--- nvim/lua/Frillion/plugins/nvim-dap-python.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "mfussenegger/nvim-dap-python",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local dap_python = require("dap-python")
-
-        dap_python.setup("~/.virtualenvs/debugpy/bin/python")
-    end,
-}
