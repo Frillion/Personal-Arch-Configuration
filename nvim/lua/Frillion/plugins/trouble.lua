153fab08780e7f5ae687e33d6ceef66d30459295
Frillion <fridriksoebech@gmail.com>
2024-08-05 21:24:58 +0000

    trying to fix gitmodule on main

--- nvim/lua/Frillion/plugins/trouble.lua
+++ nvim/lua/Frillion/plugins/trouble.lua
@@ -1,12 +1,23 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-  keys = {
-    { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+    opts = {
+        modes = {
+            errors = {
+                mode = "diagnostics",
+                filter = {
+                    severity = vim.diagnostic.severity.ERROR,
+                    function(item)
+                        return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+                    end,
+                },
+            },
+        },
+    },
+    cmd = "Trouble",
+    keys = {
+    { "<leader>te", "<cmd>Trouble errors toggle<cr>", desc = "Open/close trouble list" },
+    { "<leader>tD", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble in the current buffer" },
+    { "<leader>tq", "<cmd>Trouble qflist toggle<CR>", desc = "Open trouble quickfix list" },
   },
 }
